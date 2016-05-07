e1926a3cf59ff959cd4873607f5b250817f4bea5
James Halliday <mail@substack.net>
2016-05-07 03:21:19 +0200

    attach files to reports

---
+++ index.js
@@ -0,0 +1,60 @@
+var hypercore = require('hypercore')
+var hyperkv = require('hyperkv')
+var sub = require('subleveldown')
+var randombytes = require('randombytes')
+var once = require('once')
+var symgroup = require('symmetric-protocol-group')
+
+var CORE = 'c', KV = 'k'
+var KEYS = {
+  start: ['start'],
+  end: ['end'],
+  lon: ['location','lon'],
+  lat: ['location','lat']
+}
+
+module.exports = Monitor
+
+function Monitor (opts) {
+  if (!(this instanceof Monitor)) return new Monitor(opts)
+  this.log = opts.log
+  this.kv = hyperkv({
+    db: sub(opts.db, KV),
+    log: this.log
+  })
+  this.core = hypercore(sub(opts.db, CORE))
+}
+
+Monitor.prototype.create = function (doc) {
+  var self = this
+  var report = new Report(doc, {
+    core: self.core
+  })
+  report.once('commit', function (next) {
+    var id = randombytes(8).toString('hex')
+    self.kv.put(id, {
+      report: doc,
+      files: report.files
+    }, next)
+  })
+  return report
+}
+
+Monitor.prototype.get = function (id, cb) {
+  this.kv.get(id, cb)
+}
+
+Monitor.prototype.replicate = function (opts) {
+  return symgroup({
+    core: this.core.replicate(opts),
+    log: this.log.replicate(opts)
+  })
+}
+
+Monitor.prototype.query = function (bbox) {
+}
+
+Monitor.prototype.geojson = function (bbox) {
+}
+
+function noop () {}
